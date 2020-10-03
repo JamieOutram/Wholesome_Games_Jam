d820d2e425ea82954c1b7e8c4c494a52adbfd6a1
Luke Woolley <35926332+lukegwoolley@users.noreply.github.com>
2020-10-03 21:53:11 +0100

    if you get tuna the game will exit, put in place holder for the end screen instead of exit

---
+++ Assets/Sprites/tunapickup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tunapickup : MonoBehaviour
+{
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("tuna");
+        if (other.name == "cat")
+        {
+            Application.Quit();
+            //SceneManager.LoadScene("Game Over");
+        }
+    }
+
+}
