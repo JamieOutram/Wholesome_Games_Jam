a295280e91cb45c27034638db96b96267d0de288
JamieOutram <jamie.outram@btinternet.com>
2020-10-03 10:57:41 +0100

    Add cat body stretch visual

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //aiming state -> power state
+            //if in aiming state freeze arrow position and start power state
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            //power state -> stretch state
+            //trigger stretch with direction and power
+
+        }
+    }
+}
---
+++ Assets/Scripts/StretchBody.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StretchBody : MonoBehaviour
+{
+    public Transform feet;
+    public Transform head;
+    SpriteRenderer sprite;
+    public float bodySizeOffset;
+
+    private void Awake()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = (head.position - feet.position)/2 + feet.position;
+        transform.rotation = Quaternion.Euler(0,0, Vector2.SignedAngle(Vector2.up, head.position - feet.position));
+        sprite.size = new Vector2(1, (head.position - feet.position).magnitude + bodySizeOffset);
+
+    }
+}
