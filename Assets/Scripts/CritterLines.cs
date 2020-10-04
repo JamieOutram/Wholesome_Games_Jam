29a7ba6d91e273eccb38440c1485a40e39edad4a
JamieOutram <jamie.outram@btinternet.com>
2020-10-04 17:25:20 +0100

    add CritterLines

--- Assets/Scripts/CritterLines.cs
+++ Assets/Scripts/CritterLines.cs
@@ -4,5 +4,67 @@ using UnityEngine;
 
 public static class CritterLines
 {
-    
+    public static string[,] wormLines = new string[,]
+    {
+        {
+            "Worms First Line",
+            "Worms Second Line",
+        },
+        {
+            "Worms Second Convo",
+            "Second Convo Second Line",
+        }
+    };
+
+    public static string[,] pidgeLines = new string[,]
+    {
+        {
+            "Worms First Line",
+            "Worms Second Line",
+        },
+        {
+            "Worms Second Convo",
+            "Second Convo Second Line",
+        }
+    };
+    public static string[,] doreenLines = new string[,]
+    {
+        {
+            "Worms First Line",
+            "Worms Second Line",
+        },
+        {
+            "Worms Second Convo",
+            "Second Convo Second Line",
+        }
+    };
+    public static string[,] squirrelLines = new string[,]
+    {
+        {
+            "Worms First Line",
+            "Worms Second Line",
+        },
+        {
+            "Worms Second Convo",
+            "Second Convo Second Line",
+        }
+    };
+    public static string[,] catbroLines = new string[,]
+    {
+        {
+            "Worms First Line",
+            "Worms Second Line",
+        },
+        {
+            "Worms Second Convo",
+            "Second Convo Second Line",
+        }
+    };
+
+    public static int catbroCount = 0;
+    public static int wormCount = 0;
+
+    public static int squirrelCount = 0;
+    public static int doreenCount = 0;
+    public static int pidgeCount = 0;
 }
--- Assets/Sprites/encouragement.cs
+++ Assets/Sprites/encouragement.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum Critter
 {
@@ -16,9 +17,9 @@ public class encouragement : MonoBehaviour
     public Canvas messageCanvas;
     public Critter whatAmI;
     public int convoNumber;
-
+    public Text text;
     private int lineNumber;
-
+    private string line;
     
 
     void Start()
@@ -38,6 +39,7 @@ public class encouragement : MonoBehaviour
     private void TurnOnMessage()
     {
         messageCanvas.enabled = true;
+        UpdateMessage();
     }
 
     void OnTriggerExit2D(Collider2D other)
@@ -58,21 +60,48 @@ public class encouragement : MonoBehaviour
         switch (whatAmI)
         {
             case Critter.catbro:
-                switch (convoNumber)
+                line = CritterLines.catbroLines[convoNumber, CritterLines.catbroCount];
+                CritterLines.catbroCount++;
+                if (CritterLines.catbroCount >= CritterLines.catbroLines.GetLength(1))
                 {
-
+                    CritterLines.catbroCount = 0;
                 }
-                
                 break;
             case Critter.doreen:
+                line = CritterLines.doreenLines[convoNumber, CritterLines.doreenCount];
+                CritterLines.doreenCount++;
+                if (CritterLines.doreenCount >= CritterLines.doreenLines.GetLength(1))
+                {
+                    CritterLines.doreenCount = 0;
+                }
                 break;
             case Critter.pidge:
+                line = CritterLines.pidgeLines[convoNumber, CritterLines.pidgeCount];
+                CritterLines.pidgeCount++;
+                if (CritterLines.pidgeCount >= CritterLines.pidgeLines.GetLength(1))
+                {
+                    CritterLines.pidgeCount = 0;
+                }
                 break;
             case Critter.squirrel:
+                line = CritterLines.squirrelLines[convoNumber, CritterLines.squirrelCount];
+                CritterLines.squirrelCount++;
+                if (CritterLines.squirrelCount >= CritterLines.squirrelLines.GetLength(1))
+                {
+                    CritterLines.squirrelCount = 0;
+                }
                 break;
             case Critter.worm:
+                line = CritterLines.wormLines[convoNumber, CritterLines.wormCount];
+                CritterLines.wormCount++;
+                if (CritterLines.wormCount >= CritterLines.wormLines.GetLength(1))
+                {
+                    CritterLines.wormCount = 0;
+                }
                 break;
         }
+        text.text = line;
+
     }
 
 }
