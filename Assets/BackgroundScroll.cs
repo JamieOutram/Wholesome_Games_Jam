c354803389d2e1d2c21dd3436936a4de15f9d93a
JamieOutram <jamie.outram@btinternet.com>
2020-10-04 16:52:49 +0100

    Finished Critter Speech bubble

--- Assets/BackgroundScroll.cs
+++ Assets/BackgroundScroll.cs
@@ -21,5 +21,6 @@ public class BackgroundScroll : MonoBehaviour
     {
         Debug.Log((camera.position.y - minHeight) / maxHeight);
         transform.localPosition = new Vector2(Mathf.Lerp(endPos, startPos, (camera.position.y - minHeight) / maxHeight), 0);
+        
     }
 }
--- Assets/Sprites/encouragement.cs
+++ Assets/Sprites/encouragement.cs
@@ -2,13 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Critter
+{
+    pidge,
+    doreen,
+    squirrel,
+    worm,
+    catbro,
+}
+
 public class encouragement : MonoBehaviour
 {
     public Canvas messageCanvas;
+    public Critter whatAmI;
+    public int convoNumber;
+
+    private int lineNumber;
+
+    
 
     void Start()
     {
         messageCanvas.enabled = false;
+
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -26,7 +42,7 @@ public class encouragement : MonoBehaviour
 
     void OnTriggerExit2D(Collider2D other)
     {
-        if (other.name == "cat")
+        if (other.CompareTag("Head"))
         {
             TurnOffMessage();
         }
@@ -35,6 +51,28 @@ public class encouragement : MonoBehaviour
     private void TurnOffMessage()
     {
         messageCanvas.enabled = false;
+    }
+
+    private void UpdateMessage()
+    {
+        switch (whatAmI)
+        {
+            case Critter.catbro:
+                switch (convoNumber)
+                {
+
+                }
+                
+                break;
+            case Critter.doreen:
+                break;
+            case Critter.pidge:
+                break;
+            case Critter.squirrel:
+                break;
+            case Critter.worm:
+                break;
+        }
     }
 
 }
